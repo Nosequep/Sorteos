047538693aee437f5ea5a9465a328c2fc9c399bd
Jesús Abraham Valera Valdez <51667008+Nosequep@users.noreply.github.com>
2021-12-06 18:13:40 -0800

    Update consultarSorteo.test.js

--- src/tests/consultarSorteo.test.js
+++ src/tests/consultarSorteo.test.js
@@ -1,1 +1,19 @@
+describe('Buscar sorteo por id', () => {
+    test('suma 1+2', () => {
+        expect(1 + 2).toBe(3);
+    });
+    
+    test('resta 1-2', () => {
+        expect(1 - 2).toBe(-1);
+    });
+});
 
+describe('Buscar sorteos por título', () => {
+    test('suma 1+2', () => {
+        expect(1 + 2).toBe(3);
+    });
+    
+    test('resta 1-2', () => {
+        expect(1 - 2).toBe(-1);
+    });
+});
